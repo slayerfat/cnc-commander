821ab5d49cc56ae38c5317eaa1bc451e0ed0d4ed
Alejandro Granadillo <slayerfat@gmail.com>
2017-04-21 12:40:54 -0400

    feat: Initial commit

---
+++ app/src/renderer/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="#app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import store from 'renderer/vuex/store';
+
+  export default {
+    store
+  };
+</script>
---
+++ app/src/renderer/components/LandingPageView.vue
@@ -0,0 +1,14 @@
+<template>
+  <landing-hero></landing-hero>
+</template>
+
+<script>
+  import LandingHero from './LandingPageView/LandingHero';
+
+  export default {
+    components: {
+      LandingHero
+    },
+    name: 'landing-page'
+  };
+</script>
