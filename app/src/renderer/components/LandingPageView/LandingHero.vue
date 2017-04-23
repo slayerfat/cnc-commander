14fb7d765e5f289b5fddcff490984d7bce2635ea
Alejandro Granadillo <slayerfat@gmail.com>
2017-04-22 21:35:41 -0400

    fix: templates wrapped in section tags

--- app/src/renderer/components/LandingPageView/LandingHero.vue
+++ app/src/renderer/components/LandingPageView/LandingHero.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="container">
-    <section class="hero is-medium is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            CNC commander
-          </h1>
-          <h2 class="subtitle">
-            Algo algo algo...
-          </h2>
+  <section class="section">
+    <div class="container">
+      <section class="hero is-medium is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              CNC commander
+            </h1>
+            <h2 class="subtitle">
+              Algo algo algo...
+            </h2>
+          </div>
         </div>
-      </div>
-    </section>
-  </div>
+      </section>
+    </div>
+  </section>
 </template>
 
 <script>
--- app/src/renderer/components/UserView/UserIndexView.vue
+++ app/src/renderer/components/UserView/UserIndexView.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="container">
-    <table class="table">
-      <thead>
-      <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th><abbr title="Correo Electrónico">Email</abbr></th>
-      </tr>
-      </thead>
-      <tfoot>
-      <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th><abbr title="Correo Electrónico">Email</abbr></th>
-      </tr>
-      </tfoot>
-      <tbody>
-      <tr v-for="user in users">
-        <th>{{ user.id }}</th>
-        <td>{{ user.name }}</td>
-        <td>{{ user.email }}</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Usuarios en el sistema</h1>
+      <router-link to="/users/create" class="button">Crear nuevo usuario</router-link>
+
+      <hr>
+
+      <table class="table">
+        <thead>
+        <tr>
+          <th>Id</th>
+          <th>Nombre</th>
+          <th><abbr title="Correo Electrónico">Email</abbr></th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+          <th>Id</th>
+          <th>Nombre</th>
+          <th><abbr title="Correo Electrónico">Email</abbr></th>
+        </tr>
+        </tfoot>
+        <tbody>
+        <tr v-for="user in users">
+          <th>{{ user.id }}</th>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
 </template>
 
 <script>
-  import { UserRepository } from '../../../database/repositories/user';
+  import { UserRepository } from '../../../database/repositories/UserRepository';
 
   export default {
     data() {
