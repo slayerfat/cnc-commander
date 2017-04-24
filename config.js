6df13e82b4e9cd9e9f6bae3c1a57024674ce850b
Alejandro Granadillo <slayerfat@gmail.com>
2017-04-24 12:48:41 -0400

    feat: app can be build into 32 or 64 arch.

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ let config = {
   port: 9080,
 
   // electron-packager options
-  // Docs: https://simulatedgreg.gitbooks.io/electron-vue/content/docs/building_your_app.html
+  // Docs: https://simulatedgreg.gitbooks.io/electron-vue/content/en/building_your_app.html
   building: {
-    arch: 'x64',
+    arch: process.env.PLATFORM_ARCH || 'ia32',
     asar: true,
     dir: path.join(__dirname, 'app'),
     icon: path.join(__dirname, 'app/icons/icon'),
