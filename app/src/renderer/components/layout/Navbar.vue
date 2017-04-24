ff77efc78fff71a4f48e28c0643d78636f169b29
Alejandro Granadillo <slayerfat@gmail.com>
2017-04-23 23:46:24 -0400

    feat: add basic toggle for small displays.

--- app/src/renderer/components/layout/Navbar.vue
+++ app/src/renderer/components/layout/Navbar.vue
@@ -3,20 +3,44 @@
     <div class="container">
       <div class="nav-left">
         <router-link to="/" class="nav-item">CNC Commander</router-link>
-        <router-link to="/" class="nav-item is-hidden-mobile is-tab" exact>Inicio</router-link>
-        <router-link to="/users" class="nav-item is-hidden-mobile is-tab" exact>Usuarios</router-link>
+        <router-link to="/" class="nav-item is-hidden-mobile is-tab" exact>
+          <i class="fa fa-home"></i>&nbsp;Inicio
+        </router-link>
+        <router-link to="/users" class="nav-item is-hidden-mobile is-tab" exact>
+          <i class="fa fa-users"></i>&nbsp;Usuarios
+        </router-link>
       </div>
-      <span class="nav-toggle">
+
+      <span class="nav-toggle" @click="toggleNavMenu">
         <span></span>
         <span></span>
         <span></span>
       </span>
+    </div>
+
+    <div class="nav-right nav-menu" :class="{'is-active': isNavMenuActive}">
+      <router-link to="/" class="nav-item is-hidden-tablet" exact>
+        <i class="fa fa-home"></i>&nbsp;Inicio
+      </router-link>
+      <router-link to="/users" class="nav-item is-hidden-tablet" exact>
+        <i class="fa fa-users"></i>&nbsp;Usuarios
+      </router-link>
     </div>
   </nav>
 </template>
 
 <script>
   export default {
-    name: 'navbar'
+    name: 'navbar',
+    data() {
+      return {
+        isNavMenuActive: false
+      };
+    },
+    methods: {
+      toggleNavMenu() {
+        this.isNavMenuActive = !this.isNavMenuActive;
+      }
+    }
   };
 </script>
